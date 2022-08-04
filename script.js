70771e1faae69e46f42eb0c7a331ae02f55e11fc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-04 10:26:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//looks better in full page ...
+
+
+// BASED ON THIS LOVELY DRIBBLE --> https://dribbble.com/shots/3304775--Made-You-Look-36-Read-between-the-lines
+
+//GO LOOK AT THE ENTIRE PROJECT!
